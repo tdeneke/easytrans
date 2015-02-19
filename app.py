cdc0b3f0d33fe7ea08e6f8216e2086509653ee98
neuromancer <neuromancer@neuromancer.abo.fi>
2015-02-19 13:09:35 +0200

    added dynamic urls

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "easytrans helloworld!"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if  __name__ == '__main__':
     app.run()
