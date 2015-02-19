9a1ad62ddaffe7bfb698d5ff1a15737397200317
neuromancer <neuromancer@neuromancer.abo.fi>
2015-02-19 13:50:56 +0200

    added config

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
+    print os.environ['APP_SETTINGS']
     return "easytrans helloworld!"
 
 @app.route('/<name>')
